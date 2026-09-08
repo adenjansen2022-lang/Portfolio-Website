fb2b26787bc13306dc907854a3d7fdb728da9723
Aden Jansen <adenjansen2022@gmail.com>
2026-09-09 00:09:53 +0200

    added when user touches anywhere on the screen then the menu goes away

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector("#menu-icon");
 const navlinks = document.querySelector(".nav-links");
 const navlinksItems = document.querySelectorAll(".nav-links li a");
+const nav = document.querySelector("#nav");
 menu.addEventListener("click", () => {
   navlinks.classList.toggle("active");
 });
@@ -8,4 +9,9 @@ navlinksItems.forEach((Link) => {
   Link.addEventListener("click", () => {
     navlinks.classList.remove("active");
   });
+});
+document.addEventListener("click", (e) => {
+  if (!nav.contains(e.target)) {
+    navlinks.classList.remove("active");
+  }
 });
